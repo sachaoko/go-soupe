ffe885b2b27dac1c57224cd848910ac2858fc4a7
sachaoko <sacha.okonowski@gmail.com>
2016-04-07 17:24:10 +0200

    wip

--- includes/header.php
+++ includes/header.php
@@ -3,11 +3,30 @@
 <head>
     <meta charset="utf-8">
     <title>Go soupe</title>
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="css/style.css">
     <!-- Compiled and minified CSS -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.6/css/materialize.min.css">
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <script type="text/javascript" src="http://code.jquery.com/jquery-1.7.1.min.js"></script>
 
     <!-- Compiled and minified JavaScript -->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.6/js/materialize.min.js"></script>
 </head>
   <body>
+<!--
+navigation
+-->
+<nav class="deep-orange lighten-1">
+    <div class="nav-wrapper container deep-orange lighten-1">
+        <a href="#" class="brand-logo"><img src="img/logooms64blanc.png" alt="Un kiwi en SVG">
+        </a>
+        <ul id="nav-mobile" class="right hide-on-med-and-down">
+            <li><a href="nos-soupes.php">Nos soupes</a>
+            </li>
+            <li><a href="notre-histoire.php">Notre histoire</a>
+            </li>
+            <li><a href="contact.php">Nous contacter</a>
+            </li>
+        </ul>
+    </div>
+</nav>  
--- index.php
+++ index.php
@@ -1,6 +1,119 @@
-<?php include("includes/header.php"); ?>
-Oh my soup
+<?php include( "includes/header.php"); ?>
+
+
+<!--
+Slider
+-->
+<div class="slider">
+    <ul class="slides">
+        <li>
+            <img src="img/soupe1.jpg">
+            <!-- random image -->
+            <!--   <div class="caption left-align">
+            <h3>This is our big Tagline!</h3>
+            <h5 class="light grey-text text-lighten-3">Here's our small slogan.</h5>
+            </div>-->
+        </li>
+    </ul>
+</div>
+
+<div class="container">
+    <div class="row">
+        <h2 class="center-align"> La soupe notre métier</h2>
+        <p class="center-align">Veggies es bonus vobis, proinde vos postulo essum magis kohlrabi welsh onion daikon amaranth tatsoi tomatillo melon azuki bean garlic.</p>
+
+        <p class="center-align">Gumbo beet greens corn soko endive gumbo gourd. Parsley shallot courgette tatsoi pea sprouts fava bean collard greens dandelion okra wakame tomato. Dandelion cucumber earthnut pea peanut soko zucchini.</p>
+
+        <p class="center-align">Turnip greens yarrow ricebean rutabaga endive cauliflower sea lettuce kohlrabi amaranth water spinach avocado daikon napa cabbage asparagus winter purslane kale. Celery potato scallion desert raisin horseradish spinach carrot soko. Lotus root water spinach fennel kombu maize bamboo shoot green bean swiss chard seakale pumpkin onion chickpea gram corn pea. Brussels sprout coriander water chestnut gourd swiss chard wakame kohlrabi beetroot carrot watercress. Corn amaranth salsify bunya nuts nori azuki bean chickweed potato bell pepper artichoke.</p>
+
+    </div>
+    <div class="row">
+        <div class="icons-container">
+            <div class="col s4">
+                <div class="center promo promo-example">
+                    <i class="material-icons icons-home">stars</i>
+                    <p class="promo-caption">Soupe Premium</p>
+                    <p class="light center">We did most of the heavy lifting for you to provide a default stylings that incorporate our custom components.</p>
+                </div>
+            </div>
+            <div class="col s4">
+                <div class="center promo promo-example">
+                    <i class="material-icons icons-home">shopping_cart</i>
+                    <p class="promo-caption">Votre soupe en ligne</p>
+                    <p class="light center">By utilizing elements and principles of Material Design, we were able to create a framework that focuses on User Experience.</p>
+                </div>
+            </div>
+            <div class="col s4">
+                <div class="center promo promo-example">
+                    <i class="material-icons icons-home">thumb_up</i>
+                    <p class="promo-caption">La soupe c'est cool </p>
+                    <p class="light center">We have provided detailed documentation as well as specific code examples to help new users get started.</p>
+                </div>
+            </div>
+        </div>
+    </div>
 
 
 
-<?php include("includes/footer.php"); ?>
+
+
+    <div class="row">
+        <div class="col s3">
+            <div class="card">
+                <div class="card-image">
+                    <img src="img/soupe_p1.jpg">
+                    <span class="card-title">Card Title</span>
+                </div>
+                <div class="card-content">
+                    <p>I am a very simple card. I am good at containing small bits of information. I am convenient because I require little markup to use effectively.</p>
+                </div>
+                <div class="card-action">
+                    <a href="#">This is a link</a>
+                </div>
+            </div>
+        </div>
+        <div class="col s3">
+            <div class="card">
+                <div class="card-image">
+                    <img src="img/soupe_p1.jpg">
+                    <span class="card-title">Card Title</span>
+                </div>
+                <div class="card-content">
+                    <p>I am a very simple card. I am good at containing small bits of information. I am convenient because I require little markup to use effectively.</p>
+                </div>
+                <div class="card-action">
+                    <a href="#">This is a link</a>
+                </div>
+            </div>
+        </div>
+        <div class="col s3">
+            <div class="card">
+                <div class="card-image">
+                    <img src="img/soupe_p1.jpg">
+                    <span class="card-title">Card Title</span>
+                </div>
+                <div class="card-content">
+                    <p>I am a very simple card. I am good at containing small bits of information. I am convenient because I require little markup to use effectively.</p>
+                </div>
+                <div class="card-action">
+                    <a href="#">This is a link</a>
+                </div>
+            </div>
+        </div>
+        <div class="col s3">
+            <div class="card">
+                <div class="card-image">
+                    <img src="img/soupe_p1.jpg">
+                    <span class="card-title">Card Title</span>
+                </div>
+                <div class="card-content">
+                    <p>I am a very simple card. I am good at containing small bits of information. I am convenient because I require little markup to use effectively.</p>
+                </div>
+                <div class="card-action">
+                    <a href="#">This is a link</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<?php include( "includes/footer.php"); ?>
